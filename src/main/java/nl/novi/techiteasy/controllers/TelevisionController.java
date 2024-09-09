94099c1f89febb580e1d3a270db1cc43f52d40d5
Bernardthe3rd <bammeijer@outlook.com>
2024-09-09 11:04:19 -0700

    first push for test

---
+++ src/main/java/nl/novi/techiteasy/controllers/TelevisionController.java
@@ -0,0 +1,34 @@
+package nl.novi.techiteasy.controllers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TelevisionController {
+
+    @GetMapping("/televisions")
+    public ResponseEntity<String> getAllTelevisions() {
+        return ResponseEntity.ok("television");
+    }
+
+    @GetMapping("/television/{id}")
+    public ResponseEntity<String> getTelevisionById(@PathVariable String id) {
+        return ResponseEntity.ok("television1");
+    }
+
+    @PostMapping("/televisions")
+    public ResponseEntity<String> addTelevision() {
+        return ResponseEntity.created(null).body("television");
+    }
+
+    @PutMapping("/televisions/{id}")
+    public ResponseEntity<String> updateTelevision(@PathVariable String id) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/televisions/{id}")
+    public ResponseEntity<String> deleteTelevision(@PathVariable String id) {
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/nl/novi/techiteasy/exceptions/ExceptionController.java
@@ -0,0 +1,22 @@
+package nl.novi.techiteasy.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = {RecordNotFoundException.class})
+    public ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException exception) {
+        String message = exception.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException() {
+        String message = "Unexpected error";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
