c15f7c932ea483ac587e142be31dc0f4045b6183
Bernardthe3rd <bammeijer@outlook.com>
2024-09-09 11:06:57 -0700

    finished homework les 10 controller

--- src/main/java/nl/novi/techiteasy/controllers/TelevisionController.java
+++ src/main/java/nl/novi/techiteasy/controllers/TelevisionController.java
@@ -1,6 +1,5 @@
 package nl.novi.techiteasy.controllers;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
