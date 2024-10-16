05083a086c7d5591125f7af3881e3a4a7e0497ec
Bernardthe3rd <bammeijer@outlook.com>
2024-10-16 13:55:52 +0200

    added 3 new models

---
+++ src/main/java/nl/novi/techiteasy/models/User.java
@@ -0,0 +1,47 @@
+package nl.novi.techiteasy.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @OneToMany(
+            targetEntity = Role.class,
+            mappedBy = "username",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private Set<Role> authorities = new HashSet<>();
+
+    @Column(nullable = false)
+    private boolean enabled;
+    @Column
+    private String email;
+    @Column
+    private String apiKey;
+
+    public void addAuthority(Role role) {
+        this.authorities.add(role);
+    }
+
+    public void removeAuthority(Role role) {
+        this.authorities.remove(role);
+    }
+
+}
